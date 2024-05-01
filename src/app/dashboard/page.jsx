0cb1a7eab620aa7c6332050f5a4b1eac50ff66c6
Ashiqul Islam <bishaltheboss20@gmail.com>
2024-05-01 20:45:07 +0600

    implemented dashboard page and many more page

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,17 +1,72 @@
-// components/DashboardLayout.js
-import React from 'react';
+"use client"
+import React, {useToken} from 'react';
+import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
+import { Layout, Menu, theme } from 'antd';
+const { Header, Content, Footer, Sider } = Layout;
+const items = [UserOutlined, VideoCameraOutlined, UploadOutlined, UserOutlined].map(
+  (icon, index) => ({
+    key: String(index + 1),
+    icon: React.createElement(icon),
+    label: `nav ${index + 1}`,
+  }),
+);
 
-const DashboardLayout = ({ children }) => {
+const page = () => {
+
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+
   return (
     <div>
-    
-      <h1>Hello</h1>
-    
-      <main>{children}</main>
-
-     
+       <Layout>
+      <Sider
+        breakpoint="lg"
+        collapsedWidth="0"
+        onBreakpoint={(broken) => {
+          console.log(broken);
+        }}
+        onCollapse={(collapsed, type) => {
+          console.log(collapsed, type);
+        }}
+      >
+        <div className="demo-logo-vertical" />
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} items={items} />
+      </Sider>
+      <Layout>
+        {/* <Header
+          style={{
+            padding: 0,
+            background: colorBgContainer,
+          }}
+        /> */}
+        <Content
+          style={{
+            // margin: '24px 16px 0',
+          }}
+        >
+          <div
+            style={{
+              // padding: 24,
+              height: "100vh",
+              background: colorBgContainer,
+              borderRadius: borderRadiusLG,
+            }}
+          >
+            content
+          </div>
+        </Content>
+        {/* <Footer
+          style={{
+            textAlign: 'center',
+          }}
+        >
+          Ant Design ©{new Date().getFullYear()} Created by Ant UED
+        </Footer> */}
+      </Layout>
+    </Layout>
     </div>
   );
 };
 
-export default DashboardLayout;
+export default page;
--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Loginpage from './(auth)/login/page';
 
 const HomePage = () => {
+ 
   return (
     <div>
       <Loginpage/>
